d867cbdba2c13c817b29331c7c9f8d946800c4ad
massiliatazit <massiliatazit@users.noreply.github.com>
2021-02-03 17:55:51 +0100

    fav

--- src/reducers/jobs.js
+++ src/reducers/jobs.js
@@ -4,8 +4,15 @@ const jobsReducer = (state = [], action) => {
         return action.jobs;
       case 'LOAD_MORE_JOBS':
         return [...state, ...action.jobs];
-      default:
+        case "ADD_TO_FAVOURITES":
+      return [
+        ...state,...action.favorites]
+      
+    case "REMOVE_FROM_FAVOURITES":
+      return [
+        ...state.filter((id)=> action.payload !=id)]
+    
+    default:
         return state;
-    }
-  };
+  }}
   export default jobsReducer;
